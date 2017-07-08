6f1d059e83e2fd76bb5e192bdc6bc0d21eefabf8
tgxh2020 <tgxh2020@gmail.com>
2017-07-08 22:14:47 +0800

    readme

--- app.js
+++ app.js
@@ -4,17 +4,16 @@
 const express = require('express')
 const app = express()
 
-const query = require('./utils/utils')
 // 跨域设置
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-  res.header("X-Powered-By", ' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8")
-  next()
-})
+// app.all('*', function (req, res, next) {
+//   res.header("Access-Control-Allow-Credentials", true)
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With")
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+//   res.header("X-Powered-By", ' 3.2.1')
+//   res.header("Content-Type", "application/json;charset=utf-8")
+//   next()
+// })
 
 app.get('/', function (req, res) {
   res.send('hello express')
@@ -33,24 +32,3 @@ const port = process.env.PORT || 3333
 app.listen(3333, () => {
   console.log(`server running @${port}`)
 })
-
-
-/*function pattern(num) {
-  for (var i = 0; i < num; i++) {
-    let a = `1${'*'.repeat(i)}${i + 1}`
-    console.log(i < 1 ? a.replace('11', '1') : a)
-  }
-}
-
-pattern(10)
-
-function aaa(num) {
-  for (var i = 0; i < num; i++) {
-    let a = `${'1'.padEnd(i + 1, '*')}${i + 1}`
-    console.log(i < 1 ? a.replace('11', '1') : a)
-  }
-}
-
-function star(tel) {
-  return `${tel.substr(0,3)}${'*'.repeat(4)}${tel.substr(7)}`
-}*/
